cae2cc1b915c788a5b0e936527bd929b8bdd4a47
Lokesh-333 <lokeshtkr333@gmail.com>
2025-08-08 12:57:59 +0530

    first code in this course

---
+++ Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample{
+	public static void main(String[] args){
+		System.out.println("Hello, World!");
+	}
+}
+
+// Just like C, comments start from the two forward slashes.
+// to run java code
+// Step-1 : javac file.java
+// step-2 : java file
+
+// or direct step : java file.java (modern)
