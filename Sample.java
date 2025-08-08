b5f92f16e04aafa307da5d45cd7c20b983b7c62d
Lokesh-333 <lokeshtkr333@gmail.com>
2025-08-08 13:56:08 +0530

    1st lesson over

--- Sample.java
+++ Sample.java
@@ -1,13 +1,26 @@
 
 public class Sample{
-	public static void main(String[] args){
-		System.out.println("Hello, World!");
+	public static void main(String[] args){ // main method
+		System.out.print("\tI love pizza");
+		System.out.println("\"It's really good!");
+		System.out.print("or is it?...\n");
+		System.out.      print("---");
+
+		
 	}
 }
 
-// Just like C, comments start from the two forward slashes.
+// Just like C, single line comments start from the two forward slashes.
 // to run java code
 // Step-1 : javac file.java
 // step-2 : java file
 
 // or direct step : java file.java (modern)
+// java SE means Standard Edision
+// print statement does not add a new line but println prints in new line.
+// note that println adds new line at the end of the text, not beggining.
+
+/*
+is this a 
+multi-line comment?
+*/
